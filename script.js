88aae22db7ec42bd21fd9da13184fe16b73e9837
ero-sannin2 <rider.dragon320@gmail.com>
2025-02-14 22:37:38 +0545

    add input interface

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
- console.log("testing js")
+function getComputerChoice(){
+    return Math.floor(Math.random()*3)
+}
+function getHumanChoice(){
+    return prompt("enter your choice:")
+}
+console.log(getComputerChoice())
+console.log(getHumanChoice())
+
+let HumanScore=0
+let ComputerScore=0
+
+function playRound(computerChoice,humanChoice){
+    console.log("")
+}
+
+console.log(("hello").strcmpi("hEllo"))
