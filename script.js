b71501b1596cbeb28d76dd818826e4727824ac9d
ero-sannin2 <rider.dragon320@gmail.com>
2025-02-17 15:49:11 +0545

    create function playGame

--- script.js
+++ script.js
@@ -6,59 +6,76 @@ function getHumanChoice(){
 }
 
 
-let HumanScore=0
-let ComputerScore=0
+function playGame(){
 
-
-computerChoice=getComputerChoice()
-console.log(computerChoice)
-humanChoice=getHumanChoice()
-
-function playRound(computerChoice,humanChoice){
-    if(humanChoice=="ROCK"){
-        switch(computerChoice){
-            case 0:
-                console.log("Its a tie!!")
-                break
-            case 1:
-                console.log("The winner is computer!!")
-                ComputerScore++
-                break
-            case 2:
-                console.log("The winner is human!!")
-                HumanScore++
+    let HumanScore=0
+    let ComputerScore=0
+    function playRound(computerChoice,humanChoice){
+        if(humanChoice=="ROCK"){
+            switch(computerChoice){
+                case 0:
+                    console.log("Its a tie!!")
+                    break
+                case 1:
+                    console.log("The winner is computer!!")
+                    ComputerScore++
+                    break
+                case 2:
+                    console.log("The winner is human!!")
+                    HumanScore++
+            }
+                
         }
-            
-    }
-    else if(humanChoice=="PAPER"){
-        switch(computerChoice){
-            case 0:
-                console.log("The winner is Human!!")
-                HumanScore++
-                break
-            case 1:
-                console.log("Its a tie!!")
-                break
-            case 2:
-                console.log("The winner is computer!!")
-                ComputerScore++
+        else if(humanChoice=="PAPER"){
+            switch(computerChoice){
+                case 0:
+                    console.log("The winner is Human!!")
+                    HumanScore++
+                    break
+                case 1:
+                    console.log("Its a tie!!")
+                    break
+                case 2:
+                    console.log("The winner is computer!!")
+                    ComputerScore++
+            }
         }
-    }
-    else if(humanChoice=="SCISSOR"){
-        switch(computerChoice){
-            case 0:
-                console.log("The winner is Computer!!")
-                ComputerScore++
-                break
-            case 1:
-                console.log("The winner is Human!!")
-                break
-            case 2:
-                console.log("Its a tie")
+        else if(humanChoice=="SCISSOR"){
+            switch(computerChoice){
+                case 0:
+                    console.log("The winner is Computer!!")
+                    ComputerScore++
+                    break
+                case 1:
+                    console.log("The winner is Human!!")
+                    HumanScore++
+                    break
+                case 2:
+                    console.log("Its a tie")
+            }
         }
     }
+    
+    
+    for(let i=0;i<5;i++){
+        computerChoice=getComputerChoice()
+        console.log(computerChoice)
+        humanChoice=getHumanChoice()
+        playRound(computerChoice,humanChoice)    
+    }
+    console.log("Final Scores: ")
+    console.log("Human: ",HumanScore)
+    console.log("Computer: ",ComputerScore)
+    if(HumanScore>ComputerScore)
+        console.log("Human takes the win")
+    else if(ComputerScore>HumanScore)
+        console.log("Computer takes the win")
+    else
+        console.log("Its a draw")
+
+    HumanScore=0
+    ComputerScore=0
+
+    
 }
 
-playRound(computerChoice,humanChoice)
-console.log("Human: "+HumanScore)
-console.log("Computer: "+ComputerScore)
