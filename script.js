93ef010b01bcde7a6c7556f475e2a15e79eb102f
ero-sannin2 <rider.dragon320@gmail.com>
2025-02-17 14:59:36 +0545

    add conditionals to script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice(){
     return Math.floor(Math.random()*3)
 }
 function getHumanChoice(){
-    return prompt("enter your choice:")
+    return prompt("enter your choice:").trim().toUpperCase()
 }
 console.log(getComputerChoice())
 console.log(getHumanChoice())
@@ -10,8 +10,54 @@ console.log(getHumanChoice())
 let HumanScore=0
 let ComputerScore=0
 
+
+computerChoice=getComputerChoice()
+humanChoice=getHumanChoice()
+
 function playRound(computerChoice,humanChoice){
-    console.log("")
+    if(humanChoice=="ROCK"){
+        switch(computerChoice){
+            case 0:
+                console.log("Its a tie!!")
+                break
+            case 1:
+                console.log("The winner is computer!!")
+                ComputerScore++
+                break
+            case 2:
+                console.log("The winner is human!!")
+                HumanScore++
+        }
+            
+    }
+    else if(humanChoice=="PAPER"){
+        switch(computerChoice){
+            case 0:
+                console.log("The winner is Human!!")
+                HumanScore++
+                break
+            case 1:
+                console.log("Its a tie!!")
+                break
+            case 2:
+                console.log("The winner is computer!!")
+                ComputerScore++
+        }
+    }
+    else if(humanChoice=="SCISSOR"){
+        switch(computerChoice){
+            case 0:
+                console.log("The winner is Human!!")
+                HumanScore++
+                break
+            case 1:
+                console.log("Its a tie!!")
+                break
+            case 2:
+                console.log("The winner is computer!!")
+                ComputerScore++
+        }
+    }
 }
 
 console.log(("hello").strcmpi("hEllo"))
