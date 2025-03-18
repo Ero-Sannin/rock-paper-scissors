8bcea2246d7d18fe089249c5fd5fcafe8451442d
ero-sannin2 <rider.dragon320@gmail.com>
2025-03-18 21:26:49 +0545

    fix playRound function

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function playRound(event) {
     const computerChoice = getComputerChoice();
     console.log(computerChoice);
 
-    if(computerScore===0&&humanScore===0&&tieCount===0){
-        result.innerHTML="";
+    if(computerScore===5||humanScore===5){
+        result.innerText="";
+        humanScore=0;
+        computerScore=0;
     }
 
     if (humanChoice == "ROCK") {
         switch (computerChoice) {
             case 0:
                 result.innerText += "\nIts a tie!!";
+                tieCount++;
                 break
             case 1:
                 result.innerText += "\nThe winner is Computer";
@@ -30,6 +33,7 @@ function playRound(event) {
                 break
             case 1:
                 result.innerText += "\nIts a tie!!";
+                tieCount++;
                 break
             case 2:
                 result.innerText += "\nThe winner is Computer";
@@ -48,19 +52,19 @@ function playRound(event) {
                 break
             case 2:
                 result.innerText += "\nIts a tie!!";
+                tieCount++;
         }
     }
 
+    hScore.innerText = `Human Score: ${humanScore}`
+    cScore.innerText = `Computer Score: ${computerScore}`;
+
     if (computerScore === 5 || humanScore === 5) {
         if (humanScore < computerScore) {
-            result.innerText = "Computer wins the match";
-            humanScore = 0;
-            computerScore = 0;
+            result.innerText = "\nComputer wins the match";
         }
         else {
-            result.innerText = "Human wins the match";
-            humanScore = 0;
-            computerScore = 0;
+            result.innerText = "\nHuman wins the match";
         }
     }
 }
@@ -72,16 +76,35 @@ function getComputerChoice() {
 
 let options = document.querySelectorAll(".option");
 let result = document.querySelector(".result");
+let hScore = document.querySelector("#humanScore");
+let cScore = document.querySelector("#computerScore");
+
+
 options.forEach(option => {
     option.addEventListener("click", playRound);
+    option.addEventListener("mouseover",changeBackground)
+    option.addEventListener("mouseout",changeBackground)
 
 });
+
+function changeBackground(event){
+    if(event.type=="mouseover"){
+        event.target.style.backgroundColor="#007BFF";
+    }
+    else if(event.type="mouseout"){
+        event.target.style.backgroundColor="#007bffd8";
+    }
+}
 
 
 
 let humanScore = 0;
 let computerScore = 0;
-let tieCount=0;
+let tieCount = 0;
+
+
+hScore.innerText = `Human Score: ${humanScore}`
+cScore.innerText = `Computer Score: ${computerScore}`;
 
 
 
